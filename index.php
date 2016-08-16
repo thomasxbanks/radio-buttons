26569af2f59c344814f0465f0bdc3287a8be5524
Thomas Banks <thomasxbanks@gmail.com>
2016-08-16 11:47:33 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Radio Buttons</title>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
+	<link rel="stylesheet" href="http://sandbox.scrummable.com/style.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="form_column">
+	<h2>Choose your sammich</h2>
+	<br><br>
+	<div class="checkbox_wrapper"><input id="sammich[ts]" type="radio" name="sammich" value="ts" tabindex="3" checked="checked"><label for="sammich[ts]"><span></span> Tuna Sweetcorn</label></div>
+	<div class="checkbox_wrapper"><input id="sammich[bh]" type="radio" name="sammich" value="bh" tabindex="4"><label for="sammich[bh]"><span></span> Beef & Horseradish</label></div>
+	<div class="checkbox_wrapper"><input id="sammich[fe]" type="radio" name="sammich" value="fe" tabindex="5"><label for="sammich[fe]"><span></span> Full English</label></div>
+	<div class="checkbox_wrapper"><input id="sammich[nb]" type="radio" name="sammich" value="nb" tabindex="6"><label for="sammich[nb]"><span></span> Nutella & Banana</label></div>
+	<div class="checkbox_wrapper"><input id="sammich[tc]" type="radio" name="sammich" value="tc" tabindex="7"><label for="sammich[tc]"><span></span> Teriyaki Chicken Mayo</label></div>
+	<div class="checkbox_wrapper"><input id="sammich[ca]" type="radio" name="sammich" value="ca" tabindex="8"><label for="sammich[ca]"><span></span> Chicken & Avocado Salad</label></div>
+	<div class="checkbox_wrapper"><input id="sammich[sb]" type="radio" name="sammich" value="sb" tabindex="9"><label for="sammich[sb]"><span></span> Sausage Butty</label></div>
+</div>
+</body>
+</html>
